2b8b591c2a1208a132d3af1a4c39ff1d0217601f
明神学 <myo@myoujingakunonotopasokon.local>
2023-06-30 22:36:05 +0900

    fix styles and transform logic

--- index.js
+++ index.js
@@ -9,21 +9,21 @@ function generateStickyElements() {
   const layout = {
     parts: [
       {
-        x: "50vw",
-        y: "15vw",
-        angle: "30deg",
+        x: "523px",
+        y: "456px",
+        angle: "-12deg",
         type: "text",
         value: "Visual<br>Communication<br>Design",
-        fontSize: "2vw",
+        fontSize: "7vw",
         classes: ["font-anthony", "color-orange"],
       },
       {
-        x: "60vw",
-        y: "10vw",
-        angle: "15deg",
+        x: "1101px",
+        y: "159px",
+        angle: "26deg",
         type: "text",
         value: "Open<br>CamPus",
-        fontSize: "1vw",
+        fontSize: "4vw",
         classes: ["font-anthony", "color-blue"],
       },
       // {x: 0, y: 0, angle: 0, type: 'image', value: 'char-de.svg', width: '100px', height: '100px'}
@@ -34,32 +34,45 @@ function generateStickyElements() {
       const sticky = document.createElement("div");
       sticky.innerHTML = part.value;
       sticky.setAttribute("data-top", part.y);
-      sticky.style.top = part.y;
-      sticky.style.left = part.x;
-      sticky.style.transform = "rotate(" + part.angle + ")";
+      sticky.style.top = getAdjustedPixel(part.y) + "px";
+      sticky.style.left = getAdjustedPixel(part.x) + "px";
+      sticky.style.transform = `rotate(${part.angle})`;
       sticky.style.fontSize = part.fontSize;
       sticky.classList.add("paper");
-      part.classes.forEach(cls => {
+      part.classes.forEach((cls) => {
         sticky.classList.add(cls);
-      })
-      
+      });
+
       container.append(sticky);
     }
   });
 }
 
 function fixStickyElementPositions() {
-  const screenWidth = document.documentElement.clientWidth;
   elms.forEach((elm) => {
     const dataTop = elm.getAttribute("data-top");
-    const dataTopVw = dataTop.slice(0, dataTop.length - 2);
-    const dataTopPixel = (dataTopVw * screenWidth) / 100;
+    let dataTopPixel = getAdjustedPixel(dataTop);
+    if (dataTopPixel === null) return;
+
     if (window.scrollY < dataTopPixel) {
-      elm.style.top = dataTop;
+      elm.style.top = dataTopPixel + "px";
       elm.style.position = "absolute";
     } else {
       elm.style.top = 0;
       elm.style.position = "fixed";
     }
   });
+}
+
+function getAdjustedPixel(size) {
+  const screenWidth = document.documentElement.clientWidth;
+  if (size.endsWith("px")) {
+    const dataTopPxAs1920 = size.slice(0, size.length - 2);
+    return (dataTopPxAs1920 * screenWidth) / 1920;
+  } else if (size.endsWith("vw")) {
+    const dataTopVw = size.slice(0, size.length - 2);
+    return (dataTopVw * screenWidth) / 100;
+  } else {
+    return null;
+  }
 }
