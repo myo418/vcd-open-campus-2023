ab8a80fda5c359bb22a1279cf943e0f0e8a4443d
明神学 <myo@myoujingakunonotopasokon.local>
2023-07-01 03:24:52 +0900

    add papers

--- generate.js
+++ generate.js
@@ -2,64 +2,114 @@ generateStickyElements();
 
 function generateStickyElements() {
   const container = document.getElementById("container");
+  const texts = {
+    visualCommunicationDesign: "Visual<br>Communication<br>Design",
+    nozoite: "のぞいてみよう、視デづくり!"
+  }
+  const classes = {
+    colorBlue: 'color-blue',
+    colorOrange: 'color-orange',
+    fontAnthony: 'font-anthony'
+  }
 
   const layout = {
-    parts: [
+    texts: [
       {
         top: "456px",
         left: "523px",
         angle: "-12deg",
-        type: "text",
-        value: "Visual<br>Communication<br>Design",
+        value: texts.visualCommunicationDesign,
         fontSize: "7vw",
-        classes: ["font-anthony", "color-orange"],
+        classes: [classes.fontAnthony, classes.colorBlue],
+      },
+      {
+        top: "188px",
+        left: "38px",
+        angle: "10deg",
+        value: texts.nozoite,
+        fontSize: "1vw",
+        classes: [classes.fontAnthony, classes.colorOrange],
+      },
+      {
+        top: "232px",
+        left: "26px",
+        angle: "10deg",
+        value: texts.nozoite,
+        fontSize: "1.5vw",
+        classes: [classes.fontAnthony, classes.colorOrange],
+      },
+      {
+        top: "308px",
+        left: "12px",
+        angle: "5deg",
+        value: texts.nozoite,
+        fontSize: "2vw",
+        classes: [classes.fontAnthony, classes.colorOrange],
       },
       {
         top: "159px",
         left: "1101px",
         angle: "26deg",
-        type: "text",
         value: "Open<br>CamPus",
         fontSize: "4vw",
-        classes: ["font-anthony", "outline-blue"],
+        classes: [classes.fontAnthony, classes.colorBlue],
       },
       {
         top: "0px",
         left: "1281px",
         angle: "14deg",
-        type: "text",
         value: "2023",
         fontSize: "2vw",
-        classes: ["outline-orange"],
+        classes: [classes.colorOrange],
       },
       {
         top: "50px",
         left: "50px",
         angle: "14deg",
-        type: "text",
         value: "デ",
         fontSize: "2vw",
-        classes: ["outline-orange"],
+        classes: [classes.colorOrange],
       },
     ],
+    images: [
+      {
+        top: "100px",
+        left: "900px",
+        angle: "5deg",
+        value: "bar-shi.svg",
+        size: "4vw",
+      }
+    ]
   };
   
-  layout.parts.forEach((part) => {
-    if (part.type === "text") {
+  layout.texts.forEach((text) => {
       const sticky = document.createElement("div");
-      sticky.innerHTML = part.value;
-      sticky.setAttribute("data-top", getAdjustedVw(part.top));
-      sticky.style.top = getAdjustedVw(part.top);
-      sticky.style.left = getAdjustedVw(part.left);
-      sticky.style.transform = `rotate(${part.angle})`;
-      sticky.style.fontSize = part.fontSize;
+      sticky.innerHTML = text.value;
+      sticky.setAttribute("data-top", getAdjustedVw(text.top));
+      sticky.style.top = getAdjustedVw(text.top);
+      sticky.style.left = getAdjustedVw(text.left);
+      sticky.style.transform = `rotate(${text.angle})`;
+      sticky.style.fontSize = text.fontSize;
       sticky.classList.add("paper");
-      part.classes.forEach((cls) => {
+      text.classes.forEach((cls) => {
         sticky.classList.add(cls);
       });
 
       container.append(sticky);
-    }
+  });
+
+  layout.images.forEach((image) => {
+      const sticky = document.createElement("img");
+      sticky.setAttribute("data-top", getAdjustedVw(image.top));
+      sticky.style.top = getAdjustedVw(image.top);
+      sticky.style.left = getAdjustedVw(image.left);
+      sticky.style.transform = `rotate(${image.angle})`;
+      sticky.style.width = image.size;
+      sticky.style.height = image.size;
+      sticky.src = './images/' + image.value
+      sticky.classList.add("paper");
+
+      container.append(sticky);
   });
 }
 
